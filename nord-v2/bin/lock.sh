fc1e60733c0ac2d85fa36fd296d66d9ee80956a7
Lokesh Krishna <lokesh.krishna@pm.me>
2020-11-22 23:22:33 +0530

    Update lock screen

--- nord-v2/bin/lock.sh
+++ nord-v2/bin/lock.sh
@@ -2,37 +2,39 @@
 i3lock --nofork                 \
     --ignore-empty-password	\
     --linecolor=00000000        \
-    --keyhlcolor=88c0d0ff       \
-    --bshlcolor=d8dee9ff	\
+    --keyhlcolor=ebcb8b00       \
+    --bshlcolor=d8dee900	\
     --separatorcolor=00000000   \
-    --radius=70			\
-    --indpos="1800:960"		\
+    --radius=0			\
+    --indpos="1475:700"		\
     \
     --insidevercolor=00000000	\
     --insidewrongcolor=00000000 \
     --insidecolor=00000000	\
     \
-    --ringcolor=5e81acff        \
-    --ringvercolor=a3be8cff     \
-    --ringwrongcolor=bf616aff   \
+    --ringcolor=e59d1700        \
+    --ringvercolor=a3be8c00     \
+    --ringwrongcolor=bf616a00   \
     \
     --clock			\
     --timecolor=eceff4ff	\
-    --timestr="%H:%M"		\
-    --time-font='JetBrains Mono Nerd Font'	\
-    --timesize=75		\
-    --timepos="150:950"		\
+    --timestr="%I:%M %p"	\
+    --time-font="Calvin and Hobbes"	\
+    --timesize=125		\
+    --timepos="1425:900"		\
+    --timecolor=2e3440ff	\
     \
     --datecolor=d8dee9ff	\
     --datestr="%A, %d %B"	\
-    --date-font="JetBrains Mono Nerd Font"	\
-    --datesize=50		\
-    --datepos="340:1015"	\
+    --datecolor=2e3440ff	\
+    --date-font="Calvin and Hobbes"	\
+    --datesize=95		\
+    --datepos="tx:1025"	\
     \
     --veriftext=""		\
     --wrongtext=""		\
     \
     --indicator			\
     \
-    --image=/home/loki/pictures/wallpapers/lock.png
+    --image=/home/loki/pictures/wallpapers/nord_calvin-and-hobbes.png
 
